ce7e1338226db2e7b69bab280e2ee4374ad43e98
KIM CHAN HEE <superlipbalm@gmail.com>
2022-02-06 20:52:37 +0900

    feat: Archive 페이지 post 카드 리스트 출력

--- client/tat-ui/src/_pages/Archive.tsx
+++ client/tat-ui/src/_pages/Archive.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { useQuery } from 'urql';
 
 import Grid from '../_component/Grid';
+import PostItem from '../_component/PostItem';
+
+const PostsQuery = `
+  query {
+    posts {
+      id
+    }
+  }
+`;
 
 const Archive = () => {
-	return (
-		<div>
-			<Grid></Grid>
-		</div>
-	);
+  const [result, reexecuteQuery] = useQuery({
+    query: PostsQuery,
+    requestPolicy: 'cache-and-network',
+  });
+  const { data, fetching, error } = result;
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oops...{error.message}</p>;
+
+  return (
+    <div>
+      <Grid>
+        {data.posts.map(({ id }: { id: number }) => (
+          <PostItem ID={id} />
+        ))}
+      </Grid>
+    </div>
+  );
 };
 
 export default Archive;
