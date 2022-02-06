695569ee0c71bdfd596f49ae809f51ee8c417cbe
KIM CHAN HEE <superlipbalm@gmail.com>
2022-02-06 20:48:20 +0900

    style: globalStyle margin, padding, box-sizing 리셋

--- client/tat-ui/src/global.css.ts
+++ client/tat-ui/src/global.css.ts
@@ -1,16 +1,22 @@
 import { globalStyle } from '@vanilla-extract/css';
 
 globalStyle('html, body', {
-	padding: 0,
-	margin: 0,
-	backgroundColor: 'black',
-	color: 'white',
-	fontFamily: 'Montserrat',
-	fontWeight: 300,
+  padding: 0,
+  margin: 0,
+  backgroundColor: 'black',
+  color: 'white',
+  fontFamily: 'Montserrat',
+  fontWeight: 300,
 });
 
 globalStyle('a', {
-	color: 'white',
-	textDecoration: 'none',
-	padding: '10px 0',
+  color: 'white',
+  textDecoration: 'none',
+  padding: '10px 0',
+});
+
+globalStyle('*', {
+  boxSizing: 'border-box',
+  margin: '0',
+  padding: '0',
 });
