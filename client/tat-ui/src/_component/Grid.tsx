acc15b2bb7bfe0475942f44aded933774c9a8440
KIM CHAN HEE <superlipbalm@gmail.com>
2022-02-06 20:51:08 +0900

    feat: Grid 컴포넌트 스타일 적용 및 children 프롭 추가

--- client/tat-ui/src/_component/Grid.tsx
+++ client/tat-ui/src/_component/Grid.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { root } from './Grid.css';
 
-const Grid = () => {
-  return <div></div>;
+interface Props {
+  children: React.ReactNode;
+}
+
+const Grid = ({ children }: Props) => {
+  return <div className={root()}>{children}</div>;
 };
 
 export default Grid;
