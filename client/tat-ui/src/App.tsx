9be1e28c41254ab82d7c7c752fefee16e62985c0
KIM CHAN HEE <superlipbalm@gmail.com>
2022-02-06 20:50:29 +0900

    feat: urql 세팅 url 하드코딩 해놓은 것 나중에 바꿀 것

--- client/tat-ui/src/App.tsx
+++ client/tat-ui/src/App.tsx
@@ -5,21 +5,28 @@ import Archive from './_pages/Archive';
 import Contact from './_pages/Contact';
 import Main from './_pages/Main';
 import About from './_pages/About';
+import { createClient, Provider } from 'urql';
+
+const client = createClient({
+  url: 'http://localhost:8000/api',
+});
 
 function App() {
-	return (
-		<div className='App'>
-			<Router>
-				<Header />
-				<Routes>
-					<Route path='/' element={<Main />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/archive' element={<Archive />} />
-					<Route path='/contact' element={<Contact />} />
-				</Routes>
-			</Router>
-		</div>
-	);
+  return (
+    <Provider value={client}>
+      <div className="App">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/archive" element={<Archive />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
