56295e34f453b012c140f9252e18ad6bcc6d5382
KIM CHAN HEE <superlipbalm@gmail.com>
2022-02-06 20:52:01 +0900

    feat: PostItem 컴포넌트 캐러셀 아직 없음

--- client/tat-ui/src/_component/PostItem.css.ts
+++ client/tat-ui/src/_component/PostItem.css.ts
@@ -1,2 +1,63 @@
-import { style } from "@vanilla-extract/css";
+import { style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
 
+export const postItemWraper = recipe({
+  base: {
+    position: 'relative',
+    width: '268px',
+    height: '334px',
+    filter: 'drop-shadow(0px 3px 4px rgba(0, 0, 0, 0.25))',
+  },
+});
+
+export const postItem = recipe({
+  base: {
+    position: 'absolute',
+    left: '0%',
+    right: '0%',
+    top: '0%',
+    bottom: '0%',
+    background: '#FFFFFF',
+    border: '1px solid #BFBFBF',
+    boxSizing: 'border-box',
+    borderRadius: '20px',
+  },
+});
+
+export const imageCarousel = recipe({
+  base: {
+    position: 'absolute',
+    width: '100px',
+    height: '50px',
+    left: '90px',
+    top: '90px',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '20px',
+    lineHeight: '23px',
+    color: '#000000',
+  },
+});
+
+export const desc = recipe({
+  base: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: '266px',
+    height: '126px',
+    left: '0px',
+    bottom: '0px',
+    padding: '10px 20px',
+    background: '#000000',
+    borderRadius: '0px 0px 20px 20px',
+  },
+});
+
+export const uploadDate = recipe({
+  base: {
+    textAlign: 'right',
+  },
+});
--- client/tat-ui/src/_component/PostItem.tsx
+++ client/tat-ui/src/_component/PostItem.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import { useQuery } from 'urql';
+import { desc, imageCarousel, postItem, postItemWraper, uploadDate } from './PostItem.css';
 
-const PostITem = () => {
-  return <div></div>;
+interface Props {
+  ID: number;
+}
+
+const PostItem = ({ ID }: Props) => {
+  const [result, reexecuteQuery] = useQuery({
+    query: `
+      query {
+        post(id: ${ID}) {
+          id
+          title
+          content
+          upload_date
+        }
+      }
+    `,
+  });
+  const { data, fetching, error } = result;
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oops...{error.message}</p>;
+
+  const { title, content, upload_date } = data.post;
+  const dateString = upload_date !== null ? new Date(+upload_date).toDateString() : 'null';
+
+  return (
+    <div className={postItemWraper()}>
+      <div className={postItem()}>
+        <div className={imageCarousel()}>Image Carousel</div>
+        <div className={desc()}>
+          <h2>{title}</h2>
+          <p>{content}</p>
+          <p className={uploadDate()}>{dateString}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default PostITem;
+export default PostItem;
