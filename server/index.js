e1df6e27aa32e15f04fb58b9b28d30be88080509
KIM CHAN HEE <superlipbalm@gmail.com>
2022-02-07 19:24:20 +0900

    feat: 야매 인증 추가 .env에 ADMIN_ID, ADMIN_PASSWORD 입력해 사용

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import path from 'path';
 import Sequelize from 'sequelize';
+import { config } from 'dotenv';
+config();
 
 import { graphqlHTTP } from 'express-graphql';
 
@@ -13,10 +16,26 @@ import NOTICE from './models/NOTICE.js';
 import PHOTO from './models/PHOTO.js';
 import REVIEW from './models/REVIEW.js';
 
+const ADMIN_ID = process.env.ADMIN_ID;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+
 const PORT = 8000;
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.post('/api/auth', (req, res, next) => {
+  const { ID, password } = req.body;
+  if (ID === ADMIN_ID && password === ADMIN_PASSWORD) {
+    return res
+      .cookie('auth', true, {
+        maxAge: 60 * 60 * 1000,
+      })
+      .send();
+  }
+  return res.status(400).send();
+});
 
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
@@ -37,20 +56,20 @@ db.PHOTO = PHOTO(sequelize, Sequelize);
   try {
     await sequelize.authenticate();
     console.log('connected');
+
+    app.use(
+      '/api',
+      graphqlHTTP({
+        schema: gqlTypes.schema,
+        query: gqlTypes.Query,
+        graphiql: true,
+      })
+    );
+
+    app.listen(PORT, () => {
+      console.log('Moonwalker server has started on port', PORT);
+    });
   } catch {
     console.log('error connecting');
   }
 })();
-
-app.use(
-  '/api',
-  graphqlHTTP({
-    schema: gqlTypes.schema,
-    query: gqlTypes.Query,
-    graphiql: true,
-  })
-);
-
-app.listen(PORT, () => {
-  console.log('Moonwalker server has started on port', PORT);
-});
